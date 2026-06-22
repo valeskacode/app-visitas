0fe160d8935a281079871a584159500f92e4c8ae
valeskacode <vleskahl@gmail.com>
2026-06-22 12:55:04 -0500

    Add files via upload

---
+++ appvisita.py
@@ -0,0 +1,21 @@
+cod     columna X
+nombres y apellidos  columna E
+cargo  columna O
+fecha de ingreso
+pregunta 1  columna AA
+pregunta 2  columna AD
+pregunta 3  columna AG
+pregunta 4  columna AJ
+pregunta 5  columna AM
+pregunta 6  columna AP
+pregunta 7  columna AS
+pregunta 8  columna AV
+pregunta 9 revisión pendiente columna AY
+pregunta 10 revisión pendiente COLUMNA BB
+pregunta 11 revisión pendiente COLUMNA BE
+pregunta 12 revisión pendiente COLUMNA BH
+----------
+PAGOS INCREMENTADOS EN DIVERSAS AGENCIAS
+8 políticas una procedimientos
+--------
+performa variable
